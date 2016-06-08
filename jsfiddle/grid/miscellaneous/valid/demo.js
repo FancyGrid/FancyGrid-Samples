26a654c87079823b871ffcbb9c009508c802f9f5
Mikhail Tatsky <mikhail.tatsky@gmail.com>
2016-06-08 09:54:40 +0300

    JSFiddle - valid sample update

--- jsfiddle/grid/miscellaneous/valid/demo.js
+++ jsfiddle/grid/miscellaneous/valid/demo.js
@@ -1,18 +1,18 @@
 var data = [
-  {name: 'Ted', surname: 'Smith', position: 'Java Developer', email: 'ted.smith@gmail.com', company: 'Electrical Systems', age: 30, knownledge: 'Java, Ruby'},
-  {name: 'Ed', surname: 'Johnson', position: 'C/C++ Market Data Developer', email: 'ed.johnson@gmail.com', company: 'Energy and Oil', age: 35, knownledge: 'C++'},
-  {name: 'Sam', surname: 'Williams', position: 'Technical Analyst', email: 'sam.williams@gmail.com', company: 'Airbus', age: 38, knownledge: ''},
-  {name: 'Alexander', surname: 'Brown', position: 'Project Manager', email: 'alexander.brown@gmail.com', company: 'Renault', age: 24, knownledge: ''},
-  {name: 'Nicholas', surname: 'Miller', position: 'Senior Software Engineer', email: 'nicholas.miller@gmail.com', company: 'Adobe', age: 33, knownledge: 'Unix, C/C++'},
-  {name: 'Andrew', surname: 'Thompson', position: 'Agile Project Manager', email: 'andrew.thompson@gmail.com', company: 'Google', age: 28, knownledge: ''},
-  {name: 'Ryan', surname: 'Walker', position: 'Application Support Engineer', email: 'ryan.walker@gmail.com', company: 'Siemens', age: 39, knownledge: 'ActionScript'},
-  {name: 'John', surname: 'Scott', position: 'Flex Developer', email: 'john.scott@gmail.com', company: 'Cargo', age: 45, knownledge: 'Flex'},
-  {name: 'James', surname: 'Phillips', position: 'Senior C++/C# Developer', email: 'james.phillips@gmail.com', company: 'Pro bugs', age: 30, knownledge: 'C/C++, Unix'},
-  {name: 'Brian', surname: 'Edwards', position: 'UNIX/C++ Developer', email: 'brian.edwards@gmail.com', company: 'IT Consultant', age: 23, knownledge: 'C/C++ Unix'},
-  {name: 'Jack', surname: 'Richardson', position: 'Ruby Developer', email: 'jack.richardson@gmail.com', company: 'Europe IT', age: 24, knownledge: 'Ruby'},
-  {name: 'Alex', surname: 'Howard', position: 'CSS3/HTML5 Developer', email: 'alex.howard@gmail.com', company: 'Cisco', age: 27, knownledge: 'CSS3/HTML5'},
-  {name: 'Carlos', surname: 'Wood', position: 'Node.js Developer', email: 'carlos.wood@gmail.com', company: 'HP', age: 36, knownledge: 'Node.js'},
-  {name: 'Adrian', surname: 'Russell', position: 'Frontend Developer', email: 'adrian.russell@gmail.com', company: 'Micro Systems', age: 31, knownledge: 'HTML, JavaScript'}
+  {name: 'Ted', surname: 'Smith',email: 'ted.smith@gmail.com', company: 'Electrical Systems', age: 30},
+  {name: 'Ed', surname: 'Johnson', email: 'ed.johnson@gmail.com', company: 'Energy and Oil', age: 35},
+  {name: 'Sam', surname: 'Williams', email: 'sam.williams@gmail.com', company: 'Airbus', age: 38},
+  {name: 'Alexander', surname: 'Brown',email: 'alexander.brown@gmail.com', company: 'Renault', age: 24},
+  {name: 'Nicholas', surname: 'Miller', email: 'nicholas.miller@gmail.com', company: 'Adobe', age: 33},
+  {name: 'Andrew', surname: 'Thompson', email: 'andrew.thompson@gmail.com', company: 'Google', age: 28},
+  {name: 'Ryan', surname: 'Walker', email: 'ryan.walker@gmail.com', company: 'Siemens', age: 39},
+  {name: 'John', surname: 'Scott', email: 'john.scott@gmail.com', company: 'Cargo', age: 45},
+  {name: 'James', surname: 'Phillips', email: 'james.phillips@gmail.com', company: 'Pro bugs', age: 30},
+  {name: 'Brian', surname: 'Edwards', email: 'brian.edwards@gmail.com', company: 'IT Consultant', age: 23},
+  {name: 'Jack', surname: 'Richardson', email: 'jack.richardson@gmail.com', company: 'Europe IT', age: 24},
+  {name: 'Alex', surname: 'Howard', email: 'alex.howard@gmail.com', company: 'Cisco', age: 27},
+  {name: 'Carlos', surname: 'Wood', email: 'carlos.wood@gmail.com', company: 'HP', age: 36},
+  {name: 'Adrian', surname: 'Russell', email: 'adrian.russell@gmail.com', company: 'Micro Systems', age: 31}
 ];
 
 $(function() {
