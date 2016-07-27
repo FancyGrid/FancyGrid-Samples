6213580dba42367e30d596899e87e1c9a9ac7a32
Mikhail Tatsky <mikhail.tatsky@gmail.com>
2016-07-27 11:30:55 +0300

    JSFiddle - login sample

---
+++ jsfiddle/grid/form/login/demo.js
@@ -0,0 +1,54 @@
+$(function() {
+
+  function changeFn(){
+    var values = form.get();
+    
+    if(values.login && values.password){
+      form.buttons[1].enable();
+    }
+    else{
+      form.buttons[1].disable();
+    }
+  }
+
+  var form = new FancyForm({
+    renderTo: 'container',
+    title: 'Login',
+    width: 290,
+    height: 250,
+    items: [{
+      label: 'Login',
+      type: 'string',
+      name: 'login',
+      emptyText: 'Username or email address',
+      events: [{
+        change: changeFn
+      }]
+    },{
+      label: 'Password',
+      type: 'password',
+      emptyText: 'Password',
+      name: 'password',
+      events: [{
+        change: changeFn
+      }]
+    },{
+      inputLabel: 'Remember me?',
+      type: 'checkbox',
+      name: 'remember',
+      value: true
+    }],
+    buttons: ['side', {
+      text: 'Clear',
+      handler: function(){
+        this.clear();
+      }
+    },{
+      text: 'Login',
+      handler: function(){
+        
+      }
+    }]
+  });
+
+});
