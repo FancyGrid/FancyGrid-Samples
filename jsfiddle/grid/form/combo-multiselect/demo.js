aab8de4c99b57965f8ccb51cdb361bdbc9c83bb5
Mikhail Tatsky <mikhail.tatsky@gmail.com>
2017-08-20 11:15:54 +0300

    JSFiddle: Combo Multi Select sample

---
+++ jsfiddle/grid/form/combo-multiselect/demo.js
@@ -0,0 +1,36 @@
+$(function() {
+
+new FancyForm({
+  renderTo: 'container',
+  title: 'Combo MultiSelect',
+  width: 285,
+  height: 300,
+  items: [{
+    type: 'combo',
+	multiSelect: true,
+    label: 'MultiSelect',
+	data: [
+      {index: 'us', country: 'USA'},
+      {index: 'ca', country: 'Canada'},
+      {index: 'br', country: 'Brazil'}
+    ],
+    displayKey: 'country',
+    valueKey: 'index',
+    value: ['us', 'br']
+  },{
+    type: 'combo',
+	multiSelect: true,
+	itemCheckBox: true,
+    label: 'CheckBoxes',
+	data: [
+      {index: 'us', country: 'USA'},
+      {index: 'ca', country: 'Canada'},
+      {index: 'br', country: 'Brazil'}
+    ],
+    displayKey: 'country',
+    valueKey: 'index',
+    value: ['us', 'br']
+  }]
+});
+
+});
